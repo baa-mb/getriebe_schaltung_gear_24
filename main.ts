44963087f855601d7e638970a05982ba90cbda18
baa <34665120+baa-mb@users.noreply.github.com>
2024-11-05 13:34:44 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,3 @@
-radio.onReceivedString(function (receivedString) {
-    if (receivedString == "A") {
-        basic.showString(receivedString)
-
-        control.raiseEvent(
-            EventBusSource.MICROBIT_ID_BUTTON_A,
-            EventBusValue.MICROBIT_BUTTON_EVT_CLICK
-        )
-    } else if (receivedString == "B") {
-        basic.showString(receivedString)
-
-        control.raiseEvent(
-            EventBusSource.MICROBIT_ID_BUTTON_B,
-            EventBusValue.MICROBIT_BUTTON_EVT_CLICK
-        )
-    } else if (receivedString == "AB") {
-        basic.showString("-")
-
-        control.raiseEvent(
-            EventBusSource.MICROBIT_ID_BUTTON_AB,
-            EventBusValue.MICROBIT_BUTTON_EVT_CLICK
-        )
-    }
-})
-
-
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     on_off = !(on_off)
     if (on_off) {
@@ -39,52 +13,56 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(pos)
 })
 input.onButtonPressed(Button.AB, function () {
-    pins.servoWritePin(AnalogPin.P0, speed)
-    pos = 3
-    pins.servoWritePin(AnalogPin.P1, wlist[pos])
+    on_off = !(on_off)
+    if (on_off) {
+        pins.servoWritePin(AnalogPin.P0, speed)
+    } else {
+        pins.servoWritePin(AnalogPin.P0, 90)
+    }
 })
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "A") {
+        basic.showString(receivedString)
         control.raiseEvent(
-            EventBusSource.MICROBIT_ID_BUTTON_A,
-            EventBusValue.MICROBIT_BUTTON_EVT_CLICK
+        EventBusSource.MICROBIT_ID_BUTTON_A,
+        EventBusValue.MICROBIT_BUTTON_EVT_CLICK
         )
-    }
-    if (receivedString == "B") {
+    } else if (receivedString == "B") {
+        basic.showString(receivedString)
         control.raiseEvent(
-            EventBusSource.MICROBIT_ID_BUTTON_B,
-            EventBusValue.MICROBIT_BUTTON_EVT_CLICK
+        EventBusSource.MICROBIT_ID_BUTTON_B,
+        EventBusValue.MICROBIT_BUTTON_EVT_CLICK
         )
-    }
-    if (receivedString == "AB") {
+    } else if (receivedString == "AB") {
+        basic.showString("-")
         control.raiseEvent(
-            EventBusSource.MICROBIT_ID_BUTTON_AB,
-            EventBusValue.MICROBIT_BUTTON_EVT_CLICK
+        EventBusSource.MICROBIT_ID_BUTTON_AB,
+        EventBusValue.MICROBIT_BUTTON_EVT_CLICK
         )
     }
 })
 input.onButtonPressed(Button.B, function () {
     pins.servoWritePin(AnalogPin.P0, speed)
-    pos = 2
+    pos = 3
     pins.servoWritePin(AnalogPin.P1, wlist[pos])
-    basic.showNumber(pos)
 })
-let wlist: number[] = []
 let richtg = 0
-let speed = 0
 let on_off = false
-let pos = 0
-radio.setGroup(7)
+radio.setGroup(24)
 let winkel = 0
+let pos = 0
+let speed = 0
+let wlist: number[] = []
+radio.setGroup(7)
 pos = 0
 on_off = true
 speed = 120
 richtg = 1
 wlist = [
-    0,
-    90,
-    180,
-    135
+0,
+90,
+180,
+135
 ]
 let lauf = 1
 pins.servoWritePin(AnalogPin.P0, 90)
